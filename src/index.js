128d6fdb295190e13f7e57e2d63cdcb206465284
Sambit Satpathy <sambit_satpathy@apple.com>
2016-12-16 19:14:40 +0530

    Random data push

--- src/index.js
+++ src/index.js
@@ -3,19 +3,25 @@ import * as d3 from 'd3';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+
+function renderMap(){
+  let randomPopData={};
+  Object.keys(populationData.states).forEach((d)=>{
+    if(Math.floor(Math.random() * 2) === 0){
+      randomPopData[d]=populationData.states[d];
+    }
+  });
+  ReactDOM.render(
+    <App indiaData={indiaData} populationData={randomPopData}/>,
+    document.getElementById('root')
+  );
+}
+
 d3.json("data/india-states.json", (error, indiaData)=>{
   d3.json("data/populationData.json", (error, populationData)=>{
+    renderMap();
     setInterval(()=>{
-      let randomPopData={};
-      Object.keys(populationData.states).forEach((d)=>{
-        if(Math.floor(Math.random() * 2) === 0){
-          randomPopData[d]=populationData.states[d];
-        }
-      });
-      ReactDOM.render(
-        <App indiaData={indiaData} populationData={randomPopData}/>,
-        document.getElementById('root')
-      );
+      renderMap()
     },2000);
   });
 });
